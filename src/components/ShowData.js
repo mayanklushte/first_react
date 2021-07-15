01a25df3001e5113d98d8e4563071fee31103e87
mayanklushte <lushtemayank21@gmail.com>
2021-07-15 12:48:00 +0530

    used useState Hook

--- src/components/ShowData.js
+++ src/components/ShowData.js
@@ -1,12 +1,22 @@
 import "./ShowData.css";
 import Heading from "./Heading";
 import Container from "./Container";
+import React, { useState } from "react";
 
 function ShowData({ num1, num2, heading }) {
+  const [headings, setHeading] = useState(heading);
+  const [num, setNum] = useState(num1);
+  function headingChange() {
+    setNum(23);
+    setHeading("updated");
+    console.log("cliked");
+  }
+
   return (
     <Container className="data">
-      <Heading headings={heading} />
-      Result: <h5>{num1 + num2}</h5>
+      <Heading headings={headings} />
+      Result: <h5>{num + num2}</h5>
+      <button onClick={headingChange}>Change Heading</button>
     </Container>
   );
 }
